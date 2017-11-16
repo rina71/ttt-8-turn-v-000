338f2dbb2053ff9f7cf28df4df54fbcea28a912b
Yurina Shin <yurinas@gmail.com>
2017-11-16 09:44:40 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,6 +1,6 @@
 board = [" "," "," "," "," "," "," "," "," "]
 def display_board(board)
-
+  
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -10,18 +10,18 @@ end
 
 
 def input_to_index(input)
-index = input.to_i - 1
-
+  index = input.to_i - 1
+  
 end
 
 def valid_move?(board, index)
   if index.between?(0,8) && position_taken?(board,index) == false
-return true
-end
+    return true
+  end
 end
 
 def position_taken?(board, index)
-
+  
   if board[index] == " "
     return false
   end
@@ -35,14 +35,14 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
-index = input_to_index(input)
-   while valid_move?(board,index) == true
-
+  index = input_to_index(input)
+  while valid_move?(board,index) == true
+    
     move(board, index, value = "X")
     display_board
   else
     puts "Please enter 1-9:"
-  
-end
-end
+    
+  end
+
 end

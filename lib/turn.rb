8f6ffcba1d877eea57a6c6bce0b76590d77ca781
Yurina Shin <yurinas@gmail.com>
2017-11-16 10:37:12 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -40,6 +40,10 @@ def turn(board)
 
     move(board, index, "X")
     display_board(board)
+  else 
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
     
 
 
